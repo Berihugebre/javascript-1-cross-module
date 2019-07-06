5241e795f7559e3873c2013e25e7347fc60e0777
Berihugebre <50493216+Berihugebre@users.noreply.github.com>
2019-07-06 16:53:23 +0200

    parsing a and b

--- week-3/calc/event-listeners.js
+++ week-3/calc/event-listeners.js
@@ -5,13 +5,13 @@ function log_handler(){
 function a_handler(){
     var a_input = document.getElementById('inputa');
     var a_inputed = a_input.value;
-    a = Number(a_inputed);
+    a = parseFloat(a_inputed);
     console.log('a : ' + a)
 }
 function b_handler(){
     var b_input = document.getElementById('inputb');
     var b_inputed = b_input.value;
-    b = Number(b_inputed);
+    b = parseFloat(b_inputed);
     console.log('b : ' + b)
 }
 
